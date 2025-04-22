4998c1d9b07c5f4cdf781b126648f473af608684
Sihao Wong <2466031336@qq.com>
2025-04-22 14:44:55 +0800

    finished

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,9 +1,5 @@
 package gh2;
-
-// TODO: uncomment the following import once you're ready to start this portion
 import deque.Deque;
-// TODO: maybe more imports
-
 import deque.LinkedListDeque;
 
 //Note: This file will not compile until you complete the Deque implementations
