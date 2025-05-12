791959571de4fed81f69bedb0b11b1c1daa337d5
Sihao Wong <2466031336@qq.com>
2025-05-12 10:37:03 +0800

    finished

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -225,7 +225,6 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
                     size--;
                     return temp;
                 }
-
             }
         return null;
     }
