dae9d889ec22a9421bb4405b1743017f5855e8e9
Sihao Wong <2466031336@qq.com>
2025-05-27 17:24:18 +0800

    0527

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -16,36 +16,44 @@ import static gitlet.Utils.*;
 public class Repository {
     /**
      * TODO: add instance variables here.
-     *
+     * <p>
      * List all instance variables of the Repository class here with a useful
      * comment above them describing what that variable represents and how that
      * variable is used. We've provided two examples for you.
      */
-    static final File commitFolder = join(Repository.GITLET_DIR,"commits");
-    static final File blobsFolder = join(Repository.GITLET_DIR,"blobs");
-    /** The current working directory. */
+    static final File commitFolder = join(Repository.GITLET_DIR, "commits");
+    static final File blobsFolder = join(Repository.GITLET_DIR, "blobs");
+    /**
+     * The current working directory.
+     */
     public static final File CWD = new File(System.getProperty("user.dir"));
-    /** The .gitlet directory. */
+    /**
+     * The .gitlet directory.
+     */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
-    HashMap<String,String> commitSHA = new HashMap<>();
+    HashMap<String, String> commitSHA = new HashMap<>();
 
     String initialHead = "mater";
     Commit Head;
-    public void glInit() {
+
+    public static Commit glInit() {
         if (GITLET_DIR.mkdir()) {
             commitFolder.mkdir();
         } else {
             throw new GitletException("A Gitlet version-control system already exists in the current directory.");
         }
-        Commit initialCommit = new Commit("initial commit",null,null);
+        Commit initialCommit = new Commit("initial commit", null, null);
         commitFolder.mkdir();
         initialCommit.saveCommit();
-        Head = initialCommit;
-    }
-
-    public void commitAdd (String fileName){
-        File thisAddFile = join(CWD,fileName);
+        return initialCommit;
 
     }
-    /* TODO: fill in the rest of this class. */
+
+    public void addTempBlobs(String fileName) {
+        File thisAddFile = join(CWD, fileName);
+        String SHA1 = sha1(thisAddFile);
+        File newBlobs = join(blobsFolder, sha1(thisAddFile));
+
+        /* TODO: fill in the rest of this class. */
+    }
 }
